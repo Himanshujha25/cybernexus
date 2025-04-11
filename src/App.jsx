0c54e1c8d5b1ecca697a9d39619f3422c5e1677a
himanshu jha <jhahiamsnhu930@gmail.com>
2025-04-12 00:41:35 +0530

    commit second

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css"
+import CyberNexus from '../pages/CyberNexus'
+function App() {
+  return (
+    <div>
+      <CyberNexus />
+    </div>
+  )
+}
+
+export default App
